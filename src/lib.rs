f9a0643a3896c314c978e8d2bd75c6db0857cf01
Wouter Geraedts <git@woutergeraedts.nl>
2026-08-11 11:48:47 +0200

    Implemented jedec_id

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 #![warn(missing_docs)]
 
 use core::{fmt::Debug, marker::PhantomData};
+use derive_more::TryFrom;
 use embedded_hal::digital::{OutputPin, PinState};
 use embedded_storage::nor_flash::{ErrorType, NorFlashError, NorFlashErrorKind};
 
@@ -45,6 +46,7 @@ enum Command {
     ReadStatusRegister1 = 0x05,
     WriteEnable = 0x06,
     SectorErase = 0x20,
+    JedecId = 0x9F,
     UniqueId = 0x4B,
     Block32Erase = 0x52,
     Block64Erase = 0xD8,
@@ -192,4 +194,66 @@ fn command_and_address(command: u8, address: u32) -> [u8; 4] {
         ((address & 0x00FF00) >> 8) as u8,
         ((address & 0x0000FF) >> 0) as u8,
     ]
+}
+
+/// Major byte of the device identification (ID7-ID0) denoting chip capacity.
+///
+/// Note that the repr value corresponds to the Manufacturer/DeviceID command (0x90) and not the JEDEC ID (0x9F).
+/// The latter is the same value, but incremented by one.
+#[derive(Debug, Clone, Copy, TryFrom)]
+#[repr(u8)]
+#[try_from(repr)]
+pub enum MajorDeviceId {
+    /// W25X10 512kb device
+    W25_05 = 0x05,
+    /// W25[QX]10 1Mb device
+    W25_10 = 0x10,
+    /// W25[QX]20 2Mb device
+    W25_20 = 0x11,
+    /// W25[QX]40 4Mb device
+    W25_40 = 0x12,
+    /// W25[QX]80 8Mb device
+    W25_80 = 0x13,
+    /// W25[QX]16 16Mb device
+    W25_16 = 0x14,
+    /// W25[QX]32 32Mb device
+    W25_32 = 0x15,
+    /// W25[QX]64 64Mb device
+    W25_64 = 0x16,
+    /// W25Q128 128Mb device
+    W25_128 = 0x17,
+    /// W25Q256 256Mb device
+    W25_256 = 0x18,
+    /// W25Q512 512Mb device
+    W25_512 = 0x19,
+    /// W25Q01 1Gb device
+    W25_01 = 0x20,
+    /// W25Q02 2Gb device
+    W25_02 = 0x21,
+}
+
+/// Result from the
+pub struct JedecId([u8; 3]);
+
+impl JedecId {
+    /// Manufacturer byte of the [JedecId].
+    ///
+    /// Should always be 0xEF for Winbond.
+    pub const fn manufacturer(&self) -> u8 {
+        self.0[0]
+    }
+
+    /// Try to get the major device identifier ID7-0, denoting the chip capacity.
+    ///
+    /// If the ID does not match any known device, returns the **original** JEDEC Major Device ID,
+    /// which is incremented by one compared to the value returned by the Manufacturer/DeviceID command (0x90).
+    pub fn major_device_id(&self) -> Result<MajorDeviceId, u8> {
+        let b = self.0[2];
+        MajorDeviceId::try_from(b.checked_sub(1).ok_or(b)?).map_err(|_e| b)
+    }
+
+    /// Return the minor device identifier ID8-15, denoting the package and variant of the chip.
+    pub fn minor_device_id(&self) -> u8 {
+        self.0[1]
+    }
 }
